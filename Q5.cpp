cb4ec261c1a81c94b422906a6cd0902725c45d83
kategopal <102666045+kategopal@users.noreply.github.com>
2022-04-05 21:55:06 +0530

    Add files via upload

---
+++ Q5.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int dividend,divisor,quotient;
+	printf("enter the dividend\n");
+	scanf("%d",& dividend);
+	printf("enter the divisor");
+	scanf("%d",&divisor);
+	quotient=dividend/divisor;
+	printf("the result is%d",quotient);
+}
