f7d343f357b3c9e3df6bf727dda8035281c8bcd1
kategopal <102666045+kategopal@users.noreply.github.com>
2022-04-06 10:45:37 +0530

    Add files via upload

---
+++ Q10.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b,sum;
+	printf("enter the a/n");
+	scanf("%d",&a);
+	printf("enter the b\n");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("sum=%d",sum);
+}
