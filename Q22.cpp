6d24a4d525ce0fd46ba007877db1856d48e16e55
kategopal <102666045+kategopal@users.noreply.github.com>
2022-04-11 12:36:32 +0530

    Add files via upload

---
+++ Q22.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a, number(5),sum;
+	printf("enter first number");
+	scanf("%d",&num(1));
+	printf("enter second number");
+	scanf("%d",&num(2));
+	printf("enter third number");
+	scanf("%d",&num(3));
+	printf("enter fourth number");
+	scanf("%d",&num(4));
+	printf("enter fifth number");
+	scanf("%d",&num(5));
+	
+	if((number(a)%2)!=0)
+	{
+		sum=number(a);
+	}
+	
+	{
+		printf("sum of all odd values =%d,sum");
+	}
+	
+}
