c9918a0cc382fad0450d43ce43853b18c1874ba7
kategopal <102666045+kategopal@users.noreply.github.com>
2022-04-12 11:55:11 +0530

    assignment

---
+++ Q35.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int num1,num2,i,add=0;
+	i=num1;
+	printf("enter two integers");
+    scanf("%d%d",&num1,&num2);
+	while(i<=num2);
+	{
+		{
+		if(i%17!=0)
+		add=add+i;
+		printf("add=%d",add);
+	    }
+	}
+}
