71ecdd219f6df9d4fb6f71df70197dc236201ad8
kategopal <102666045+kategopal@users.noreply.github.com>
2022-04-13 20:49:50 +0530

    Add files via upload

---
+++ Q4.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	printf(" ######/n");
+	printf("#/n");
+	printf("#/n");
+	printf("######/n");
+	printf("#/n");
+	printf("#/n");
+	printf("#/n");
+	
+	printf("    ##########/n");
+	printf("   ##          ##/n");
+	printf("   #/n");
+	printf("   #/n");
+	printf("   #/n");
+	printf("   #/n");
+	printf("   ##          ##/n");
+	printf("    ##########/n");
+	
+}
