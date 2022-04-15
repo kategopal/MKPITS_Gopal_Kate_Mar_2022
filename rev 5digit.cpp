6fcd98a5ae02c84adeff77f36f7e6ee94d0df226
kategopal <102666045+kategopal@users.noreply.github.com>
2022-04-15 22:31:08 +0530

    assignment

---
+++ rev 5digit.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int num,n1,n2,n3,n4,n5,rev;
+	printf("enter 5 digit num");
+	scanf("%d",&num);
+	
+	n1=num%10;
+	num=num/10;
+	
+	n2=num%10;
+	num=num/10;
+	
+	n3=num%10;
+	num=num/10;
+	
+	n4=num%10;
+	num=num/10;
+	
+	n5=num%10;
+	
+	rev=n1*10000+n2*1000+n3*100+n4*10+n5*1;
+	
+	printf("rev=%d",rev);
+}
