752e990e2556ae8ddf6184000ea99610e5b449f7
kategopal <102666045+kategopal@users.noreply.github.com>
2022-04-16 11:23:38 +0530

    assignment

---
+++ volume of sphere.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	float radius,res,pi=3.142;
+	printf("enter radius of sphere");
+	scanf("%f",&radius);
+	
+	res=1.33*3.142*radius*radius*radius;
+	
+	printf("res=%f",res);
+	
+}
