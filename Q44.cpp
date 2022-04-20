bdb3b23565f6078cbe4e4578d2a02bdb6c0fffcd
kategopal <102666045+kategopal@users.noreply.github.com>
2022-04-20 12:10:11 +0530

    ASSI

---
+++ Q44.cpp
@@ -0,0 +1,16 @@
+//WAP TO PRINT ADD OF NUMBER FROM 10 TO 1.
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,add=0;
+	i=10;
+	do
+	{
+		{
+			add=add+i;
+			printf("add=%d ",add);
+		}
+		i--;
+	}while(i>=1);
+}
