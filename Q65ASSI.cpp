2fb41da67cb0bee88580399a71b504f237c031cd
kategopal <102666045+kategopal@users.noreply.github.com>
2022-05-02 11:26:58 +0530

    assignment

---
+++ Q65ASSI.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b;
+	printf("enter two number :");
+	scanf("%d%d",&a,&b);
+	
+	if(a%b==0)
+	{
+		printf("multiple");
+	}
+	else
+	{
+		printf("not multiple");
+	}
+	
+}
