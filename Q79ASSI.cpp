9f1889e9fbdcd4347d11fc932132f1c3b15f0795
kategopal <102666045+kategopal@users.noreply.github.com>
2022-05-02 12:06:20 +0530

    assignment

---
+++ Q79ASSI.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdio.h>
+main()
+{
+	int n;
+	printf("enter the value");
+	scanf("%d",&n);
+	
+	if(n%3==0)
+	{
+		printf("div of 3");
+	}
+	if(n%7==0)
+	{
+		printf("div of 7");
+	}
+}
