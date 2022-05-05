368fb7267a7cef67706beef0728eb4b0465454d4
kategopal <102666045+kategopal@users.noreply.github.com>
2022-05-05 10:39:08 +0530

    ASSI

---
+++ string upper case convert.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i;
+	char ch1[78];
+	printf("enter any name in inupper case :");
+	scanf("%s",ch1);
+	for(i=0;ch1[i]!='\0';i++)
+	{
+		printf("%c",ch1[i]+32);
+	}
+}
