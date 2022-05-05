582d5031247244043a715557dcfbab7c4ad88021
kategopal <102666045+kategopal@users.noreply.github.com>
2022-05-05 10:22:45 +0530

    ASSI

---
+++ second type of function (10).cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void fun(int);
+int main()
+{
+	int num;
+	fun(num);
+}
+void fun(int num)
+{
+	int i;
+	for(i=1;i<=10;i++)
+	{
+		if(i%2!=0)
+		{
+			printf("\n%d",i);
+		}
+	}
+}
