b19b1a517cc78f01418f0d978d13b05b304790b1
kategopal <102666045+kategopal@users.noreply.github.com>
2022-05-12 10:32:36 +0530

    ASSI

---
+++ Q91.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b;
+	printf("enter the value of a :");
+	scanf("%d",&a);
+	printf("enter the value of b :");
+	scanf("%d",&b);
+	if(a+b<10||a+b<20)
+	{
+		printf("30");
+	}
+}
