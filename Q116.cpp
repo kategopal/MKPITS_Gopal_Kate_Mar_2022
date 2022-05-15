8a378fd4498aa6c3ffd43aac6fc63856c3b0984e
kategopal <102666045+kategopal@users.noreply.github.com>
2022-05-15 18:34:43 +0530

    ASSI

---
+++ Q116.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int x,y,add=0,mul=1,sub=0,div;
+	
+	printf("enter the value of x :");
+	scanf("%d",&x);
+	printf("enter the value of y :");
+	scanf("%d",&y);
+	
+	add=x+y;
+	{
+	printf("\nadd of value=%d",add);	
+	}
+	sub=x-y;
+	{
+	printf("\nsub of value=%d",sub);	
+	}
+	mul=x*y;
+	 {
+	 	printf("\nmul of value=%d",mul);
+	 }
+	
+	
+	
+}
