b33c101ff3ddced50f303b4fd8fdef0d309de272
kategopal <102666045+kategopal@users.noreply.github.com>
2022-05-16 10:06:11 +0530

    ASSI

---
+++ Q127.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdio.h>
+main()
+{
+	int a[5],i,count=0;
+	printf("enter 5 array elements :");
+	for(i=0;i<5;i++)
+	{
+		printf("a[%d]=",i);
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<5;i++)
+	{
+		if(a[i]==5 || a[i+1]==5)
+		{
+			printf("true");
+		}
+		else
+		{
+			printf("false");
+		}
+	}
+
+}
