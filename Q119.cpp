802c6e3be0632a99fc35d56088d1459221f6995c
kategopal <102666045+kategopal@users.noreply.github.com>
2022-05-19 09:28:55 +0530

    ASSI

---
+++ Q119.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a[5],i,add=0;
+	printf("enter array elements :");
+	for(i=0;i<5;i++)
+	{
+		printf("a[%d]=",i);
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<5;i++)
+	{
+		if(a[i]!=17)
+		{
+			add=add+a[i];
+		}
+	}
+	printf("%d",add);
+}
