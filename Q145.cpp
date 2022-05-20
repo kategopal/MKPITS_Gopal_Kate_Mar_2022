b6090140328c43a5f3a8f624213631fe0b6ba2c9
kategopal <102666045+kategopal@users.noreply.github.com>
2022-05-20 09:13:36 +0530

    ASSI

---
+++ Q145.cpp
@@ -0,0 +1,10 @@
+// wrute a program that takes an age (for example 20) as input and print something as "you look older than 20"
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int n;
+	printf("enter your age :");
+	scanf("%d",&n);
+	printf("you look older than %d",n);
+}
