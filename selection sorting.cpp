eab9215bc6a6d78bc02446d34f5e9ac25e81592b
kategopal <102666045+kategopal@users.noreply.github.com>
2022-05-22 12:12:11 +0530

    ASSI

---
+++ selection sorting.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a[10],i,j,temp;
+	printf("enter array elements :");
+	for(i=0;i<5;i++)
+	{
+		printf("a[%d]=",i);
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<5;i++)
+	{
+		for(j=i+1;j<5;j++)
+		{
+			if(a[i]>a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	for(i=0;i<5;i++)
+		{
+			printf("%d\n",a[i]);
+		}
+}
