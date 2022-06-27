9a4b7407538f6cb35bbe0e1e851998f8f82e8f95
kategopal <kategopal111@gmail.com>
2022-06-27 15:11:55 +0530

    find string len

---
+++ assignment/strlen.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+main()
+{
+	char g[20];
+	int len;
+	printf("enter the name :");
+	scanf("%s",g);
+	len=strlen(g);
+	printf("lenth of the name=%d",len);
+}
