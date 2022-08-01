c901405931ec8c8e48cdc4d7546c82ecd21044ff
kategopal <kategopal111@gmail.com>
2022-08-02 00:23:36 +0530

    str rev

---
+++ assignment/strrev.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+main()
+{
+	char name[20]="Hello jordan";
+	//printf("enter any name");
+	//scanf("%s",name);
+	strrev(name);
+	
+	printf("reverse of name is=%s",name);
+}
