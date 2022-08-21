30b25a34c85818a2d093cb29c65303c4c3aaa1f5
kategopal <kategopal111@gmail.com>
2022-08-21 17:25:10 +0530

    prac Q4 class 5Aug

---
+++ OOPS/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_Q4_class_5Aug
+{
+    class Employee
+    {
+        public int empno;
+        public string empname;
+        public double salary;
+        //creating methods getdata() and showdata()
+        public void getdata(int e1, string en, double sa)
+        {
+            empno = e1;
+            empname = en;
+            salary = sa;
+
+        }
+        public void showdata()
+        {
+            Console.WriteLine("empno " + empno);
+            Console.WriteLine("empname " + empname);
+            Console.WriteLine("salary " + salary);
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                //creating an instance(object) of class employee
+                //syntax classname objectname = new classname();
+                Employee emp = new Employee();
+                emp.getdata(11, "amit", 232323);
+
+                emp.showdata();
+            }
+        }
+    }
+}
