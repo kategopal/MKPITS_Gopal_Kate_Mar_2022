d3b2ac2d1da42834ca31c1826fe0272487d605f6
kategopal <kategopal111@gmail.com>
2022-09-04 11:17:33 +0530

    prac Q1 single dim Array

---
+++ WindowsForm/prac Q1 single dim Array 23Aug/prac Q1 single dim Array 23Aug/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac_Q1_single_dim_Array_23Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[]num=new int[5];
+        int sum = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                if (i < 5)
+                {
+                    num[i] = Convert.ToInt32(textBox1.Text);
+                    sum = sum + num[i];
+                    textBox1.Clear();
+                    textBox1.Focus();
+                    if (i == 5)
+                    {
+                        label2.Text = "sum of 5 Num=" + sum;
+                    }
+
+                    //    num[i] = Convert.ToInt32(textBox1.Text);
+                    //    sum = sum + num[i];
+
+                    //    textBox1.Clear();
+                    //    textBox1.Focus();
+                    //    if (i == 5)
+                    //    {
+                    //        label2.Text = "sum of 5 no." + sum;
+                    //    }
+
+                    //}
+                    //else
+                    //{
+
+                    //}
+
+                }
+            }
+        }
+    }
+}
