f87f2898be5ed4ecbc88eab594351679d80b51b8
kategopal <kategopal111@gmail.com>
2022-09-04 11:25:20 +0530

    prac q2 class Bank 25Aug

---
+++ WindowsForm/prac Q2 Class  bank  25Aug/prac Q2 Class  bank  25Aug/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac_Q2_Class__bank__25Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bank obj = new bank();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            obj. actno =Convert.ToInt32 (textBox1.Text);
+            //obj.amount=Convert.ToInt32 (textBox2.Text);
+            string dep = obj.deposit(Convert.ToInt32(textBox2.Text));
+            label3.Text = dep;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            obj.actno =Convert.ToInt32 (textBox1.Text);
+            string dep=obj.withdrawl(Convert.ToInt32(textBox2.Text));
+            label3.Text=dep;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            obj.actno=Convert.ToInt32 (textBox1.Text);
+            string dep = obj.showbalance();
+            label3.Text = dep;
+        }
+    }
+}
---
+++ WindowsForm/prac Q2 Class  bank  25Aug/prac Q2 Class  bank  25Aug/bank.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_Q2_Class__bank__25Aug
+{
+    internal class bank
+    {
+       public int actno;
+        public int amount;
+        public int bal = 1000;
+
+        public string deposit(int amount)
+        {
+            bal = bal + amount;
+            return "Diposited Amount=" + bal.ToString();
+        }
+        public string withdrawl(int amount)
+        {
+            string res = null;
+            if(amount>bal)
+            {
+                res="Withdrawl is unsuccessfull";
+            }
+            else
+            {
+                bal =amount-bal;
+                res= "balance After withdrawl="+bal.ToString();
+            }
+            return res;
+           
+        }
+        public string showbalance()
+        {
+            return "Balance Amount is=" + bal.ToString();
+        }
+
+    }
+}
