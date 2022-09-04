a199c9fb7af0daac8438c1d89933a5f438d0401b
kategopal <kategopal111@gmail.com>
2022-09-04 11:35:49 +0530

    prac Q3 Add class 25Aug

---
+++ WindowsForm/prac Q3 add class 25Aug/prac Q3 add class 25Aug/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac_Q3_add_class_25Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        account obj = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            obj.actno = Convert.ToInt32(textBox1.Text);
+            string str = obj.deposit(Convert.ToInt32(textBox2.Text));
+            label3.Text = str;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            obj.actno = Convert.ToInt32(textBox1.Text);
+            string str = obj.withdrawl(Convert.ToInt32(textBox2.Text));
+            label3.Text = str;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            obj.actno = Convert.ToInt32(textBox1.Text);
+            string str = obj.showbalance();
+            label3.Text = "account no : " + obj.actno + " : " + str;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "saving")
+            {
+                obj = new saving();
+            }
+            else if (comboBox1.Text == "current")
+            {
+                obj = new current();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
+
---
+++ WindowsForm/prac Q3 add class 25Aug/prac Q3 add class 25Aug/account.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_Q3_add_class_25Aug
+{
+    internal abstract  class account
+    {
+       public int actno;
+        public int balance = 1000;
+        // string acttype;
+
+        public abstract string deposit(int amount);
+
+        public string withdrawl(int amount)
+        {
+            string res = null;
+            if (amount > balance)
+            {
+                res = "insufficient fund , cannot withdraw money";
+            }
+            else
+            {
+                balance = balance - amount;
+                res = "amount withdrawl successfully, bal is " + balance.ToString();
+            }
+            return res;
+        }
+        public string showbalance()
+        {
+            return "balance amount is " + balance.ToString();
+        }
+
+    }
+    class saving : account
+    {
+        public override string deposit(int amount)
+        {
+            int interest = 500;
+            balance = balance + amount + interest;
+            return "amount deposited successfully with interest , bal is " + balance.ToString();
+
+        }
+    }
+    class current : account
+    {
+        public override string deposit(int amount)
+        {
+
+            balance = balance + amount;
+            return "amount deposited successfully without interest , bal is " + balance.ToString();
+
+        }
+
+    }
+}
