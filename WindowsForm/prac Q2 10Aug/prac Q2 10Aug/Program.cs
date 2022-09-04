b07a19127ba8102e6a82f38fa63d33258da457ab
kategopal <kategopal111@gmail.com>
2022-09-04 11:21:50 +0530

    prac Q2 10Aug

---
+++ WindowsForm/prac Q2 10Aug/prac Q2 10Aug/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_Q2_10Aug
+{
+    public class ParentClass
+    {
+        public ParentClass()
+        {
+            Console.WriteLine("Parent Constructor.");
+        }
+        public void print()
+        {
+            Console.WriteLine("I'm a Parent Class.");
+        }
+    }
+    public class ChildClass : ParentClass
+    {
+        public ChildClass()
+        {
+            Console.WriteLine("Child Constructor.");
+        }
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+                ChildClass child = new ChildClass();
+                child.print();
+            }
+        }
+    }
+}
