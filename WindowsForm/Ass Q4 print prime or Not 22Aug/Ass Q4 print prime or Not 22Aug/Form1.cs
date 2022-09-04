1352a69fd93e7076df98b8f1183246d40c1f9384
kategopal <kategopal111@gmail.com>
2022-09-04 10:47:57 +0530

    Ass Q4 print prime or not 22Aug

---
+++ WindowsForm/Ass Q4 print prime or Not 22Aug/Ass Q4 print prime or Not 22Aug/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ass_Q4_print_prime_or_Not_22Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(textBox1.Text);
+            int count = 0;
+            for (int i = 1; i < num; i++)
+            {
+                if(num%i== 0)
+                {
+                    count++;
+                }
+            }
+            if(count==2)
+            {
+               label2.Text ="This is prime Number";
+            }
+            else
+            {
+                label2.Text = "This is Not Prime Number";
+            }
+        }
+    }
+}
