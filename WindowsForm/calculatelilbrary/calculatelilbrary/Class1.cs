5d20a64b3481aef9db15b5c91f952301df592907
kategopal <kategopal111@gmail.com>
2022-09-04 11:01:02 +0530

    calculate library

---
+++ WindowsForm/calculatelilbrary/calculatelilbrary/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculatelilbrary
+{
+    public class calculate
+    {
+        public int num1;
+        public int num2;
+        public int res;
+
+        public string addition(int num1,int num2)
+        {
+            res = num1 + num2;
+            return res.ToString();
+        }
+        public string subtraction(int num1, int num2)
+        {
+            res = num1 - num2;
+            return res.ToString();
+        }
+    }
+}
