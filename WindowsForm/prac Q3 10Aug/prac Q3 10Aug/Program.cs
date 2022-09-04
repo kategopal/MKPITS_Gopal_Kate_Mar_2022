9cd00af6f39b13611582d0105900b8fb39bc8249
kategopal <kategopal111@gmail.com>
2022-09-04 11:34:30 +0530

    prac Q3 10aug

---
+++ WindowsForm/prac Q3 10Aug/prac Q3 10Aug/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_Q3_10Aug
+{
+    class student
+    {
+        int rno;
+        string name;
+
+        public student()
+        {
+            rno = 11;
+            name = "amitabh";
+        }
+        public student(int rno, string name)
+        {
+            this.rno = rno;
+            this.name = name;
+        }
+        public void displaydata()
+        {
+            Console.WriteLine("rno " + rno);
+            Console.WriteLine("name " + name);
+        }
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+                student s = new student(); //this will call constructor automatically
+
+                s.displaydata();
+                student s1 = new student(111, "rajesh");
+                s1.displaydata();
+
+            }
+        }
+    }
+}
