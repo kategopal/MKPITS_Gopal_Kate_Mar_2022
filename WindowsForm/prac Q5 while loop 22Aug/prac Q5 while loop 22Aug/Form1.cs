4bcb347ff8b0ee768d272d4116d9d3ba67622a04
kategopal <kategopal111@gmail.com>
2022-09-04 11:52:06 +0530

    prac Q5 while loop 22Aug

---
+++ WindowsForm/prac Q5 while loop 22Aug/prac Q5 while loop 22Aug/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac_Q5_while_loop_22Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(textBox1.Text);
+            int fact = 1;
+            int i = 1;
+            while(i <= num)
+            {
+                fact = fact * i;
+                i++;
+            }
+            label2.Text = "Factorial of Number=" + fact;
+        }
+    }
+    
+}
