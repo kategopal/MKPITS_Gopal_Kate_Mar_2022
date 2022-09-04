f4ad6e9e501791f13b7dc3185f6207801816f17d
kategopal <kategopal111@gmail.com>
2022-09-04 10:02:04 +0530

    Ass Q1 class Student 25Aug

---
+++ WindowsForm/Ass Q1 class Student 25Aug/Ass Q1 class Student 25Aug/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ass_Q1_class_Student_25Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        student obj=new student();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            obj.sub1 = Convert.ToInt32(textBox1.Text);
+            obj.sub2 = Convert.ToInt32(textBox2.Text);
+            obj.sub3 = Convert.ToInt32(textBox3.Text);
+
+            string str = obj.calculatetotal(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+            label4.Text =str;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            obj.sub1 = Convert.ToInt32(textBox1.Text);
+            obj.sub2 = Convert.ToInt32(textBox2.Text);
+            obj.sub3 = Convert.ToInt32(textBox3.Text);
+
+            string str = obj.calculatepercentage(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+            label4.Text = "percentage=" + str;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            obj.sub1 = Convert.ToInt32(textBox1.Text);
+            obj.sub2 = Convert.ToInt32(textBox2.Text);
+            obj.sub3 = Convert.ToInt32(textBox3.Text);
+
+            string str = obj.calculategrade(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+            label4.Text = "Grade= " + str;
+        }
+    }
+}
---
+++ WindowsForm/Ass Q1 class Student 25Aug/Ass Q1 class Student 25Aug/student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//1- create a class result having 3 fields chemistry, physics and maths and method calculatetotal , calculatepercentage and calculategrade design the form and use the class
+
+namespace Ass_Q1_class_Student_25Aug
+{
+    internal class student
+    {
+       public int sub1;
+        public int sub2;
+        public int sub3;
+
+        public string calculatetotal(int sub1,int sub2,int sub3)
+        {
+            int total;
+            total=sub1+sub2+sub3;
+            return "Total marks of All Subject=" + total.ToString();
+        }
+        public string calculatepercentage(int sub1,int sub2,int sub3)
+        {
+            int total;
+            total = sub1 + sub2 + sub3;
+            float per;
+            per=(total/300.0f)*100;
+            return per.ToString();
+        }
+        public string calculategrade(int sub1, int sub2, int sub3)
+        {
+            int total;
+            total = sub1 + sub2 + sub3;
+            float per;
+            per = (total / 300.0f) * 100;
+            if (per>75)
+            {
+                return "distintion";
+            }
+            else if(per>60 && per<75)
+            {
+                return "first class";
+            }
+            else if(per>50 && per<60)
+            {
+                return "second class";
+            }
+            else
+            {
+                return "fail";
+            }
+        }
+    }
+}
