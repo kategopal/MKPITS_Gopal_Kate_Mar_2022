b6d1eb0c9873eff4f7c70940f0ec378b7b451090
kategopal <kategopal111@gmail.com>
2022-09-04 10:27:12 +0530

    Ass Q3 Loop 22Aug

---
+++ WindowsForm/Ass Q3 loop 22Aug/Ass Q3 loop 22Aug/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ass_Q3_loop_22Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            int num = Convert.ToInt32(textBox1.Text);
+           
+            {
+            
+                 if(num%4==0)
+                {
+                    label2.Text = "leap year";
+                }
+                 else
+                {
+                    {
+                        label2.Text = "Not leap year";
+                    }
+                }
+            }
+           
+        }
+    }
+}
