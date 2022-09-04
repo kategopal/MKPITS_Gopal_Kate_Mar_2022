efd04397525315bee6deecb101e1153e588f9ff7
kategopal <kategopal111@gmail.com>
2022-09-04 11:37:14 +0530

    prac Q3 Add sub mul 22Aug

---
+++ WindowsForm/prac Q3 Add Sub Mul 22Aug/prac Q3 Add Sub Mul 22Aug/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac_Q3_Add_Sub_Mul_22Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1=Convert.ToInt32(textBox1.Text);
+            int num2=Convert.ToInt32(textBox2.Text);
+            int add=num1+num2;
+            label3.Text="Add of Two Number="+add;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2=Convert.ToInt32(textBox2.Text);
+            int sub = num1 - num2;
+            label3.Text = "Subtraction=" + sub;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int num1=Convert.ToInt32(textBox1.Text);
+            int num2=Convert.ToInt32(textBox2.Text);
+            int mul = num1 * num2;
+            label3.Text = "Multiplication of Number=" + mul;
+        }
+    }
+}
