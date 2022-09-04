08e21de1ea64de3147ef91e2e80a8a808ad10ad1
kategopal <kategopal111@gmail.com>
2022-09-04 11:50:35 +0530

    prac Q5 total percenteage grade 22Aug

---
+++ WindowsForm/prac Q5 total,per and grd 22Aug/prac Q5 total,per and grd 22Aug/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac_Q5_total_per_and_grd_22Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sub1=Convert.ToInt32(textBox1.Text);
+            int sub2=Convert.ToInt32(textBox2.Text);
+            int sub3=Convert.ToInt32(textBox3.Text);
+            int total=sub1+sub2+sub3;
+            label4.Text = "Total=" + total;
+            float per = (total / 300.0f) * 100;
+            label5.Text = "Percentage=" + per;
+
+            if(per>=75)
+            {
+                label6.Text = "Distinction";
+            }
+            else if(per>=60 && per<75)
+            {
+                label6.Text = "First Class";
+            }
+            else if(per>=40 && per<60)
+            {
+                label6.Text = "Second Class";
+            }
+            else
+            {
+                label6.Text = "Fail";
+            }
+        }
+    }
+}
