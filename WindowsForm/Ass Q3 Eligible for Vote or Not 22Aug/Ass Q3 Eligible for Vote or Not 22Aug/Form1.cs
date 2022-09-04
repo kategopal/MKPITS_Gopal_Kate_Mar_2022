fc856f40d46eeb249d8799e9e047fc09b685f037
kategopal <kategopal111@gmail.com>
2022-09-04 10:25:35 +0530

    Ass Q3 Eligible for vote or Not 22Aug

---
+++ WindowsForm/Ass Q3 Eligible for Vote or Not 22Aug/Ass Q3 Eligible for Vote or Not 22Aug/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ass_Q3_Eligible_for_Vote_or_Not_22Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int age=Convert.ToInt32(textBox1.Text);
+            if(age>=18)
+            {
+                label3.Text = "Eligible for Vote";
+            }
+            else
+            {
+                label3.Text = "Not Eligible for Vote";
+            }
+        }
+    }
+}
