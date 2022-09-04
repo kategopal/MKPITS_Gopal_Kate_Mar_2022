47508870998d44a3c7d362ae4aa5c1ec36ae45f1
kategopal <kategopal111@gmail.com>
2022-09-04 10:22:10 +0530

    Ass Q2 Switch 22Aug

---
+++ WindowsForm/Ass Q2 Switch 22Aug/Ass Q2 Switch 22Aug/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ass_Q2_Switch_22Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char ch=Convert.ToChar(textBox1.Text);
+            switch(ch)
+            {
+                case 'a':
+                    {
+                        label3.Text = "It is an Vowel";
+                        break;
+                    }
+                
+                case 'e':
+                    {
+                        label3.Text = "It is an Vowel";
+                        break;
+                    }
+                case 'i':
+                    {
+                        label3.Text = "It is an Vowel";
+                        break;
+                    }
+                case 'o':
+                    {
+                        label3.Text = "It is an Vowel";
+                        break;
+                    }
+                case 'u':
+                    {
+                        label3.Text = "It is an Vowel";
+                        break;
+                    }
+                case 'A':
+                    {
+                        label3.Text = "It is an Vowel";
+                        break;
+                    }
+                case 'E':
+                    {
+                        label3.Text = "It is an Vowel";
+                        break;
+                    }
+                case 'I':
+                    {
+                        label3.Text = "It is an Vowel";
+                        break;
+                    }
+                case 'O':
+                    {
+                        label3.Text = "It is an Vowel";
+                        break;
+                    }
+                case 'U':
+                    {
+                        label3.Text = "It is an Vowel";
+                        break;
+                    }
+                default:
+                    {
+                        label3.Text = "Invalid";
+                        break;
+                    }
+            }
+        }
+    }
+}
