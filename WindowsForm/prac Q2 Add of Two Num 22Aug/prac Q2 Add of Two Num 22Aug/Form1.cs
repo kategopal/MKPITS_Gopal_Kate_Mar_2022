8c071daf9a4936ea0b3fa32d07962e7cb4872726
kategopal <kategopal111@gmail.com>
2022-09-04 11:23:00 +0530

    prac Q2 Add of Two Num 22Aug

---
+++ WindowsForm/prac Q2 Add of Two Num 22Aug/prac Q2 Add of Two Num 22Aug/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac_Q2_Add_of_Two_Num_22Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1=Convert.ToInt32(textBox1.Text);
+            int num2=Convert.ToInt32(textBox2.Text);
+            int Add=num1+ num2;
+            label3.Text="Add of Number="+Add;
+        }
+    }
+}
