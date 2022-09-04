48f41de7a911335082b216c26851cdacc67eebcc
kategopal <kategopal111@gmail.com>
2022-09-04 11:45:02 +0530

    prac Q4 print greater num 22Aug

---
+++ WindowsForm/prac Q4 print greater Num 22Aug/prac Q4 print greater Num 22Aug/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac_Q4_print_greater_Num_22Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1=Convert.ToInt32(textBox1.Text);
+            int num2=Convert.ToInt32(textBox2.Text);
+            if(num1>num2)
+            {
+                label3.Text = "num1 is Greater";
+            }
+            else
+            {
+                label3.Text = "num2 is Greater";
+            }
+        }
+    }
+}
