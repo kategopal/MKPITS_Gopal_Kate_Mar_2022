5c6d98e69e228826100ec6333497fe38e8c388d3
kategopal <kategopal111@gmail.com>
2022-09-04 11:09:05 +0530

    prac Q1 10Aug

---
+++ WindowsForm/prac Q1 10Aug/prac Q1 10Aug/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_Q1_10Aug
+{
+    class student
+    {
+        private int rno;
+        public void getdata(int rno)
+        {
+            this.rno = rno;
+        }
+        public void displaydata()
+        {
+            Console.WriteLine("rno " + rno);
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                student s = new student();
+                // s.rno = 1; error
+                s.getdata(11);
+                s.displaydata();
+            }
+        }
+    }
+}
