7f0f141a0c4962cb5ba147a1dfa204a06253f92b
kategopal <kategopal111@gmail.com>
2022-09-04 11:07:34 +0530

    prac Q1 radiobutton 24Aug

---
+++ WindowsForm/pra Q1 RadioButton 24Aug/pra Q1 RadioButton 24Aug/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pra_Q1_RadioButton_24Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int bal = 1000;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int actno=Convert.ToInt32(textBox1.Text);
+            int amount=Convert.ToInt32(textBox2.Text);
+            if(radioButton1.Checked)
+            {
+                bal = bal + amount;
+            }
+            else
+            {
+                bal = bal - amount;
+            }
+            label3.Text = "Result="+bal;
+        }
+    }
+}
