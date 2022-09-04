12638b69902efa377a0c72539ab7a2625413b61c
kategopal <kategopal111@gmail.com>
2022-09-04 11:10:48 +0530

    prac Q1 add student clAss

---
+++ WindowsForm/prac Q1 Add Student Class 25Aug/prac Q1 Add Student Class 25Aug/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac_Q1_Add_Student_Class_25Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            student obj =new student();
+            obj. rno=Convert.ToInt32(textBox1.Text);
+            obj.sname=textBox2.Text;
+           obj.email=textBox3.Text;
+            obj.mob=Convert.ToInt64(textBox4.Text);
+
+            StringBuilder sb=new StringBuilder();
+            sb.Append("Roll No=" + obj.rno+"\n");
+            sb.Append("student Name=" + obj.sname + "\n");
+            sb.Append("Email=" + obj.email + "\n");
+            sb.Append("Mob No=" + obj.mob + "\n");
+            label5.Text = sb.ToString();
+        }
+    }
+}
---
+++ WindowsForm/prac Q1 Add Student Class 25Aug/prac Q1 Add Student Class 25Aug/student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_Q1_Add_Student_Class_25Aug
+{
+    internal class student
+    {
+        public int rno;
+        public string sname;
+        public string email;
+        public long mob;
+    }
+}
