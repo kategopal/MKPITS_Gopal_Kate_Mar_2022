87e8ed7810914603d74dfedb388372183ab1b852
kategopal <kategopal111@gmail.com>
2022-09-04 10:49:49 +0530

    Bank classlibrary2

---
+++ WindowsForm/bankclasslibrary2/bankclasslibrary2/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankclasslibrary2
+{
+    public abstract class account
+    {
+        public int actno;
+        public int bal = 1000;
+        public int intrest = 300;
+
+        public abstract string deposit(int amount);
+        
+        public string withdrawl(int amount)
+        {
+            bal=bal-amount;
+            return deposit(amount);
+        }
+        public string show()
+        {
+            return bal.ToString();
+        }
+    }
+    public class saving : account
+    { 
+        public override string deposit(int amount)
+        {
+            bal = bal + amount + intrest;
+            return bal.ToString();
+        }
+    }
+    public class current:account
+    {
+        public override string deposit(int amount)
+        {
+            bal=bal + amount;
+            return bal.ToString();
+        }
+    }
+
+}
