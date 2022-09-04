82151f6e62012710930eb85ec7908da3854f1464
kategopal <kategopal111@gmail.com>
2022-09-04 10:10:22 +0530

    Ass Q1 Switch 22Aug

---
+++ WindowsForm/Ass Q1 switch 22Aug/Ass Q1 switch 22Aug/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ass_Q1_switch_22Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 =Convert.ToInt32(textBox1.Text);
+            int num2=Convert.ToInt32(textBox2.Text);
+            char opt=Convert.ToChar(textBox3.Text);
+            int res = 0;
+            switch(opt)
+            {
+                case '+':
+                    {
+                        res = num1 + num2;
+                        break;
+                    }
+                case '-':
+                    {
+                        res = num1 - num2;
+                        break;
+                    }
+                case '*':
+                    {
+                        res = num1 * num2;
+                        break;
+                    }
+
+                    
+            }
+            label4.Text = "Result=" + res;
+        }
+    }
+}
