89e66de901d63d2b54c1dfcb3d62f5fa949fb92d
kategopal <kategopal111@gmail.com>
2022-09-22 20:00:01 +0530

    Ass Q1 loop 22Aug

---
+++ OOPS/Ass Q1 loop 22Aug/Ass Q1 loop 22Aug/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ass_Q1_loop_22Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            int num = Convert.ToInt32(textBox1.Text);
+            int res = 1;
+            int i = 0;
+            while (i <= 10)
+            {
+                res = num * i;
+                sb.Append(num + "*" + i + "=" + res + "\n");
+                i++;
+            }
+            label2.Text = sb.ToString();
+        }
+    }
+}
