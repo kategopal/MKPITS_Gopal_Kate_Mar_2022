4ae441c47e0e70f5bddd2c6a1f0fd28dc0798734
kategopal <kategopal111@gmail.com>
2022-09-22 20:16:04 +0530

    prac Q12 trackbar and label control 29Aug

---
+++ OOPS/prac Q12 trackbar and label control 29Aug/prac Q12 trackbar and label control 29Aug/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac_Q12_trackbar_and_label_control_29Aug
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
