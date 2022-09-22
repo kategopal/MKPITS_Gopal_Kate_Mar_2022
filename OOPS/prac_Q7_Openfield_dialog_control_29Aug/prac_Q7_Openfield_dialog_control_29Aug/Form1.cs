dd6c2f833698349e20a9d4ccaab804a3eb38ac95
kategopal <kategopal111@gmail.com>
2022-09-22 20:31:36 +0530

    prac Q7 openfield

---
+++ OOPS/prac_Q7_Openfield_dialog_control_29Aug/prac_Q7_Openfield_dialog_control_29Aug/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac_Q7_Openfield_dialog_control_29Aug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text= saveFileDialog1.Title;
+
+            }
+        }
+    }
+}
